2280e71f38a6885695993b571e810d4602992c45
DJ <social.deej@gmail.com>
2021-03-19 13:15:55 +0530

    feat : Updation Prc next to Auth

--- src/routes/v1/auth/oauth-route.ts
+++ src/routes/v1/auth/oauth-route.ts
@@ -3,6 +3,8 @@
 import { NextFunction, Request, Response } from "express";
 import express from 'express'
 import passport from 'passport'
+import User, {UserModel} from '../../../database/models/User'
+import isLoggedIn from '../../../middlewares/isloggedin'
 
 const router = express.Router();
 
@@ -15,7 +17,30 @@ router.get('/auth/google/redirect', passport.authenticate('google'), async (req:
     console.log(user)// @ts-ignore  
     var email = user.email
     res.send(user);
-});    
+});
+
+router.get('/update/userstatus/mentor', isLoggedIn,async (req: Request, res: Response) => {
+  const {background, desc, phNum, location, isMentor, org, createdAt, updatedAt} = req.body;
+  // @ts-ignore
+  const check = await UserModel.findOne({_id : req.user._id})
+
+  if (check){
+    check.background = background;
+    check.desc = desc;
+    check.phNum = phNum;
+    check.location = location;
+    check.isMentor = isMentor ;
+    check.org = org;
+    check.createdAt = createdAt;
+    check.updatedAt = updatedAt;
+
+    await check.save();
+
+    res.send(check);
+  } else 
+        res.send("User Not Found !");
+
+})
 
 
 export default router;
